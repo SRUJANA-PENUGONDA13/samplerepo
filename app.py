475a72d1b93d765f84562b9a8bdda190e8e556b5
Srujana Penugonda <srujanapenugonda1318@outlook.com>
2021-05-23 18:08:57 +0530

    Chore - Modules Update

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask
 from datetime import datetime
+from __future__ import print_function
+import pickle
+import os.path
+import pandas as pd
+import base64
+from email.mime.text import MIMEText
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email.mime.multipart import MIMEMultipart
+
 app = Flask(__name__)
 
 @app.route('/')
